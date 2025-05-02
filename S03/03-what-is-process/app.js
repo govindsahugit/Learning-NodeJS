53bb7b5802c9887b60d44c79b8f7b2b9f1b0c907
GOVIND <work.govindsahu@gmail.com>
2025-05-02 17:32:24 +0530

    complete-S03-L03

---
+++ S03/03-what-is-process/app.js
@@ -0,0 +1,13 @@
+/* 
+Process: Apps, and Programs, are also process to understand about process easily. There are various type of Processes that contain defferent kind of stages.
+1. Running Process: Application is running without any problem
+2. Waiting Process: Application is waiting for execution here application or process want to execute but some how it is waiting for execution.
+3. Sleeping Process: Application is running and completed execution 
+*/
+
+const a = process;
+console.log(process);
+
+// setInterval(() => {
+//   console.log("NodeJS");
+// }, 100);
