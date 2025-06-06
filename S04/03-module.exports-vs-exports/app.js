3450831a904c710c67a835f6115b6c475be33e39
GOVIND <work.govindsahu@gmail.com>
2025-06-06 18:53:16 +0530

    complete-04-03

---
+++ S04/03-module.exports-vs-exports/app.js
@@ -0,0 +1,7 @@
+// const sum = require("./sum");
+// const product = require("./product");
+
+const { sum, product } = require("./math");
+
+console.log(sum(1, 2, 3, 4));
+console.log(product(1, 2, 3, 4));
