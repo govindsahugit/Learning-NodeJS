c8c38d75bac2477736a6d1ebd7cb97c543559a32
GOVIND <work.govindsahu@gmail.com>
2025-06-10 12:19:00 +0530

    complete-04-19

---
+++ S04-fundamentals-of-nodejs/19-introduction-to-fs-module/app.js
@@ -0,0 +1,19 @@
+// import fs from "node:fs";c
+
+// const contentBuffer = fs.readFileSync("./hello.html");
+// const content = contentBuffer.toString()
+// console.log(content);
+
+// const content = fs.readFileSync("./hello.html", "utf-8");
+// console.log(content);
+
+// fs.readFile("./hello.html", (err, data) => {
+//   console.log(data.toString());
+// });
+
+//=================================================================//
+
+// import fs from "node:fs/promises";
+
+// const data = await fs.readFile("./hello.html");
+// console.log(data.toString());
