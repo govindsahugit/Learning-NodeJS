be84ff787452d3b0092fd60c896b44cb5521bf09
GOVIND <work.govindsahu@gmail.com>
2025-06-10 18:01:57 +0530

    complete-04-22

---
+++ S04-fundamentals-of-nodejs/22-nodejs-file-operations/app.js
@@ -0,0 +1,11 @@
+import fs from "node:fs/promises";
+
+// fs.rename("home.jpg", "image.jpg");
+
+// fs.copyFile("home.png", "C:\\Users\\Govind Sahu\\OneDrive\\Desktop\\image.png");
+
+// fs.cp("./src", "C:\\Users\\Govind Sahu\\OneDrive\\Desktop\\src", {
+//   recursive: true,
+// });
+
+// and more....
