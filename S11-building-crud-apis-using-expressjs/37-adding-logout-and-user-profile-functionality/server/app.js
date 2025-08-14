45bda8f13b3c779980e95de7d95e2883433365c8
GOVIND <work.govindsahu@gmail.com>
2025-08-14 12:35:20 +0530

    complete-11-37-and-38

---
+++ S11-building-crud-apis-using-expressjs/37-adding-logout-and-user-profile-functionality/server/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import directoryRoutes from "./routes/directoryRoutes.js";
+import fileRoutes from "./routes/fileRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
+import cookie from "cookie-parser";
+import { CheckAuth } from "./middlewares/Auth.js";
+
+const app = express();
+const port = 4000;
+
+app.use(
+  cors({
+    origin: "http://192.168.1.10:5173",
+    credentials: true,
+  })
+);
+
+app.use(cookie());
+
+app.use(express.json());
+
+app.use(express.static("storage"));
+
+app.use("/directory", CheckAuth, directoryRoutes);
+
+app.use("/file", CheckAuth, fileRoutes);
+
+app.use("/user", userRoutes);
+
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    message: "Something went wrong",
+    error: err.message,
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port 4000");
+});
