d2463b2735a29c7b8ee6d83f27c2716264c7221c
GOVIND <work.govindsahu@gmail.com>
2025-08-21 08:57:36 +0530

    complete-12-36

---
+++ S12-mastering-database-fundamentals-with-mongodb/36-fixing-ObjectId-validation-in-our-storage/server/db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient("mongodb://127.0.0.1:27017/storageApp");
+
+export const connectDB = async () => {
+  await client.connect();
+  console.log("Mongodb connected");
+  const db = client.db();
+  return db;
+};
+
+process.on("SIGINT", async () => {
+  await client.close();
+  console.log("DB client disconnected");
+  process.exit(0);
+});
