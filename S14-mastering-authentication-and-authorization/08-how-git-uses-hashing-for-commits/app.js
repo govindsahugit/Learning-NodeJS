a9ee57f554efe732654252a89cad4c7de23dc14d
GOVIND <work.govindsahu@gmail.com>
2025-09-10 18:59:02 +0530

    first test commit

---
+++ S14-mastering-authentication-and-authorization/08-how-git-uses-hashing-for-commits/app.js
@@ -0,0 +1,9 @@
+import crypto from "crypto";
+import { readFile } from "fs/promises";
+
+const fileData = await readFile("test.txt");
+
+const newData = `blob ${fileData.length}\0${fileData}`;
+
+const hash = crypto.createHash("sha1").update(newData).digest("hex");
+console.log(hash);
