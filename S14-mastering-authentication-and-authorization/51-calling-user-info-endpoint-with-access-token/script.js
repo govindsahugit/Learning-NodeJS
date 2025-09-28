5e25ba480ed0871e4c8fa2a2f2d4001b5d2dcaed
GOVIND <work.govindsahu@gmail.com>
2025-09-28 16:19:10 +0530

    complete-14-51-aultered

---
+++ S14-mastering-authentication-and-authorization/51-calling-user-info-endpoint-with-access-token/script.js
@@ -0,0 +1,14 @@
+const accessToken = "";
+
+const response = await fetch(
+  `https://openidconnect.googleapis.com/v1/userinfo?access_token=${accessToken}`
+  // {
+  //   headers: {
+  //     Authorization: `Bearer ${accessToken}`,
+  //   },
+  // }
+);
+
+const userData = await response.json();
+
+console.log(userData);
