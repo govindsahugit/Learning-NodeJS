32d9ab19f5e0aa09c044bc1cff60affc047089ea
GOVIND <work.govindsahu@gmail.com>
2025-09-29 19:22:41 +0530

    complete-14-53

---
+++ S14-mastering-authentication-and-authorization/53-error-handling-in-google-login-server/client/login/script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector("button");
+const baseURL = "http://localhost:4000";
+
+button.addEventListener("click", () => {
+  window.open(`${baseURL}/auth/google`, "auth-popup", "width=500,height=600");
+});
+
+// const res = await fetch(`${baseURL}/profile`, {
+//   credentials: "include",
+// });
+
+// if (res.status === 200) location.href = "/client/index.html";
+
+window.addEventListener("message", ({ data }) => {
+  if (data.message === "success") location.href = "/client/index.html";
+  else {
+    const p = document.createElement("p");
+    p.innerText = "Something went wrong!";
+    document.body.appendChild(p)
+    setTimeout(() => {
+      p.remove();
+    }, 2000);
+  }
+});
---
+++ S14-mastering-authentication-and-authorization/53-error-handling-in-google-login-server/server/services/googleAuthService.js
@@ -0,0 +1,33 @@
+import { OAuth2Client } from "google-auth-library";
+
+const clientId =
+  "1080347041084-ql6qrkks5ucvunc56fu3qgkkbogniuic.apps.googleusercontent.com";
+const clientSecret = "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";
+const redirectUrl = "http://localhost:4000/auth/google/callback";
+
+const client = new OAuth2Client({
+  clientId,
+  clientSecret,
+  redirectUri: redirectUrl,
+});
+
+export const generateAuthUrl = () => {
+  return client.generateAuthUrl({
+    scope: ["email", "profile", "openid"],
+    prompt: "consent",
+    // login_hint: "govind@gmail.com"
+  });
+};
+
+export async function fetchUserFromGoogle(code) {
+  const { tokens } = await client.getToken(code);
+
+  const loginTicket = await client.verifyIdToken({
+    idToken: tokens.id_token,
+    audience: clientId,
+  });
+
+  const userData = loginTicket.getPayload();
+  console.log(userData);
+  return userData;
+}
