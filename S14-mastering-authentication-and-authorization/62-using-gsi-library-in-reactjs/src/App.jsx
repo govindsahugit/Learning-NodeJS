93e3b4f8a4a4e94ee9c1d6457d1f055b78c16abf
GOVIND <work.govindsahu@gmail.com>
2025-09-30 17:57:22 +0530

    complete-14-62

---
+++ S14-mastering-authentication-and-authorization/62-using-gsi-library-in-reactjs/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { GoogleLogin } from "@react-oauth/google";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+        useOneTap
+      />
+    </>
+  );
+}
+
+export default App;
