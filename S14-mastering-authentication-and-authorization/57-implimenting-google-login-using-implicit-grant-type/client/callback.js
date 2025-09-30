7bc7540d40cc346b5ef6c14e124b6d1e56f21feb
GOVIND <work.govindsahu@gmail.com>
2025-09-30 12:31:11 +0530

    complete-14-57-and-58

---
+++ S14-mastering-authentication-and-authorization/57-implimenting-google-login-using-implicit-grant-type/client/callback.js
@@ -0,0 +1,20 @@
+const idToken = new URLSearchParams(location.hash.substring(1)).get("id_token");
+if (idToken) {
+  const baseURL = "http://localhost:4000";
+  const response = await fetch(`${baseURL}/auth/google`, {
+    credentials: "include",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ idToken }),
+  });
+
+  if (response.status === 200) {
+    window.opener.postMessage({ message: "success" });
+    window.close();
+  }
+} else {
+  window.opener.postMessage({ message: "failure" });
+  window.close();
+}
---
+++ S14-mastering-authentication-and-authorization/57-implimenting-google-login-using-implicit-grant-type/client/login/script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector("button");
+const baseURL = "http://localhost:4000";
+
+const clientId =
+  "1080347041084-ql6qrkks5ucvunc56fu3qgkkbogniuic.apps.googleusercontent.com";
+const redirectUri = "http://localhost:5500/client/callback.html";
+const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=id_token&client_id=${clientId}&scope=openid email profile&redirect_uri=${redirectUri}&nonce=123hii`;
+
+button.addEventListener("click", () => {
+  window.open(authUrl, "auth-popup", "width=500,height=600");
+});
+
+window.addEventListener("message", ({ data }) => {
+  console.log(data);
+  if (data.message === "success") location.href = "/client/index.html";
+  else {
+    const p = document.createElement("p");
+    p.innerText = "Something went wrong!";
+    document.body.appendChild(p);
+    setTimeout(() => {
+      p.remove();
+    }, 2000);
+  }
+});
