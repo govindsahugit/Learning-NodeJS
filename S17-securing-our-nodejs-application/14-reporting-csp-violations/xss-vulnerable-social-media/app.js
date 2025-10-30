a40c527b95c1720f252d6040088ff7ff421fca95
GOVIND <work.govindsahu@gmail.com>
2025-10-30 12:24:02 +0530

    complete-17-14

---
+++ S17-securing-our-nodejs-application/14-reporting-csp-violations/xss-vulnerable-social-media/app.js
@@ -0,0 +1,66 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import { JSDOM } from "jsdom";
+import DOMPurify from "dompurify";
+
+const window = new JSDOM("").window;
+const purify = DOMPurify(window);
+
+const app = express();
+
+app.use(express.json());
+
+await mongoose.connect(
+  "mongodb://admin:admin@localhost/socialApp?authSource=admin"
+);
+
+const postSchema = new mongoose.Schema({
+  content: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+app.use((req, res, next) => {
+  if (req.headers.accept?.includes("text/html"))
+    res.setHeader(
+      "Content-Security-Policy",
+      "default-src 'self';\
+  script-src 'self' 'report-sample' https://cdn.tailwindcss.com;\
+  img-src 'self' https://images.unsplash.com;\
+  style-src 'self' 'unsafe-inline';\
+  connect-src 'self';\
+  report-uri /csp-violations"
+    );
+  next();
+});
+
+// Middleware
+app.use(express.static("./public"));
+
+app.post(
+  "/csp-violations",
+  express.json({ type: "application/csp-report" }),
+  (req, res) => {
+    console.log(req.body);
+    return res.status(204).end();
+  }
+);
+
+// Routes
+app.get("/posts", async (req, res) => {
+  const posts = await Post.find().sort({ createdAt: -1 });
+  res.setHeader("Set-Cookie", "loginSecret=hdxhw7yrx.k;httpOnly=true");
+  res.json(posts);
+});
+
+app.post("/posts", async (req, res) => {
+  const content = req.body.content;
+  const post = new Post({ content });
+  await post.save();
+  res.status(201).json(post);
+});
+
+// Start server
+app.listen(4000, () => console.log("Server running on http://localhost:4000"));
