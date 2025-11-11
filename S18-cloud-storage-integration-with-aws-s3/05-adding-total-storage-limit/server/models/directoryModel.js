6e65939abd25bb26c23b3dc7ed4b12367cd4bd25
GOVIND <work.govindsahu@gmail.com>
2025-11-11 22:09:57 +0530

    complete-18-05

---
+++ S18-cloud-storage-integration-with-aws-s3/05-adding-total-storage-limit/server/models/directoryModel.js
@@ -0,0 +1,39 @@
+import { model, Schema } from "mongoose";
+
+const directorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+      select: "name email",
+    },
+    parentDirId: {
+      type: Schema.Types.ObjectId,
+      default: null,
+      ref: "Directory",
+    },
+    isPublic: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    strict: "throw",
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const Directory = model("Directory", directorySchema);
+
+export default Directory;
