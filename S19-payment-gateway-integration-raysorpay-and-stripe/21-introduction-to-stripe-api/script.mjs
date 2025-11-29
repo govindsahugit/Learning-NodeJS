2930d50b3f9e24fbddc9e258ba2d47f28a64fb78
GOVIND <work.govindsahu@gmail.com>
2025-11-29 07:30:10 +0530

    complete-19-21

--- S19-payment-gateway-integration-raysorpay-and-stripe/21-introduction-to-stripe-api/script.mjs
+++ S19-payment-gateway-integration-raysorpay-and-stripe/21-introduction-to-stripe-api/script.mjs
@@ -1,7 +1,7 @@
 const res = await fetch("https://api.stripe.com/v1/balance", {
   headers: {
     Authorization:
-      "Bearer sk_test_51SV4qOLKpJbunV8x2zmeM1sDIB2Ll7GGkrdOanCHMJDB3ZAz0AU0y506A4JuoItVAaSDtHJRNViFRnke5CAC6pFG00GyyB0TCK",
+      "Bearer secret-key",
   },
 });
 
