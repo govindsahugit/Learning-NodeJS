a1da35eb44b9df492ca99cea6219e3a6429a1000
GOVIND <work.govindsahu@gmail.com>
2026-05-24 14:24:43 +0530

    complete-22-37

---
+++ S22-observability-fundamentals-for-production-system/37-transports-in-pino/app.js
@@ -0,0 +1,37 @@
+import pino from "pino";
+
+// const logger = pino({
+//   transport: {
+//     target: "pino/file",
+//     options: {
+//       destination: "app.log",
+//       mkdir: true,
+//     },
+//   },
+// });
+
+const logger = pino({
+  transport: {
+    targets: [
+      {
+        target: "pino/file",
+        options: {
+          destination: "app.log",
+          mkdir: true,
+        },
+      },
+      {
+        target: "pino-pretty",
+        options: {
+          // destination: 1, // 1 is stdout
+          destination: "./pretty.log",
+          colorize: false,
+          // translateTime: "dd-mm-yyyy HH:MM:ss",
+          translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+        },
+      },
+    ],
+  },
+});
+
+logger.info("Hii");
