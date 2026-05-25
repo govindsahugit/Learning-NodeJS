63e98e3ad525b36f165f6076f8f936a5077a47fd
GOVIND <work.govindsahu@gmail.com>
2026-05-25 08:54:31 +0530

    complete-22-38

---
+++ S22-observability-fundamentals-for-production-system/38-redacting-sensitive-data-in-pino/app.js
@@ -0,0 +1,53 @@
+import pino from "pino";
+
+// const logger = pino({
+//   transport: {
+//     target: "pino/file",
+//     options: {
+//       destination: "app.log",
+//       mkdir: true,
+//     },
+//   },
+// });
+
+const logger = pino({
+  transport: {
+    targets: [
+      {
+        target: "pino/file",
+        options: {
+          destination: "app.log",
+          mkdir: true,
+        },
+      },
+      {
+        target: "pino-pretty",
+        options: {
+          // destination: 1, // 1 is stdout
+          destination: "./pretty.log",
+          colorize: false,
+          // translateTime: "dd-mm-yyyy HH:MM:ss",
+          translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+        },
+      },
+    ],
+  },
+  redact: {
+    paths: ["password", "address"],
+    remove: true
+  },
+});
+
+logger.info({
+  id: 123,
+  username: "John Doe",
+  email: "john@example.com",
+  password: "secret123",
+  address: {
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipcode: "10001",
+    country: "USA",
+  },
+});
